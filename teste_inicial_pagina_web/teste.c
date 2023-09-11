825417f58fb3627d63b627b1ed56a53e9df1a3a1
ProfessorRafaelKruger <144643170+ProfessorRafaelKruger@users.noreply.github.com>
2023-09-11 00:09:41 -0300

    Create teste.c

---
+++ teste_inicial_pagina_web/teste.c
@@ -0,0 +1,18 @@
+// isso é um comentário de linha, o compilador não executa isso
+/*isso é um comentário de bloco*/
+// atalho para comentários = "ctrl" + ";"
+// IDE é um ambiente de desenvolvimento integrado
+// compilador é a parte que executa (compila) o código
+// include serve para incluir biblioteca
+// stdio.h - INPUT/OUTPUT - entrada e saída para o usuário
+#include <stdio.h>
+
+/********  Começo da função main ********/
+// main - principal - função principal
+int main(void) { //void = vazio - tipo de valor
+  // printf - imprimir na tela do usuário
+  printf("Hello World\n");
+  return 0;
+} // fechar colchetes para dizer que o código acabou
+
+
